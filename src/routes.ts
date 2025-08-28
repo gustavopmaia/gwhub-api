780f1bf48ab087221e04d916103e052f3afb6709
Gustavo Maia <67283753+gustavopmaia@users.noreply.github.com>
2025-08-27 21:03:51 -0300

    healthcheck impl

--- src/routes.ts
+++ src/routes.ts
@@ -3,4 +3,5 @@ import { OpenAiController } from "./controllers/openai.controller";
 
 export const routes = async (app: FastifyInstance) => {
   app.post("/api/alexa", (req, res) => OpenAiController(req, res));
+  app.get("/api/healthcheck", (req, res) => res.send({ status: 'OK' }));
 };
